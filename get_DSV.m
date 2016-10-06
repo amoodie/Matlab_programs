3ebd538acca104dcca595ed4435bf9667bffcd1d
amoodie <amoodie@rice.edu>
2016-10-05 22:35:00 -0500

    upload DSV, fix

--- get_DSV.m
+++ get_DSV.m
@@ -41,34 +41,21 @@ function [ws, wa] = get_DSV(D, CSF, Pow, varargin)
     ws = NaN(size(D));
     
     for d = 1:length(D(:)) % for each grain size
-        Da = get_Da(D(d), con); % non-dimensionalize with constants
+        Da = ((con.rho_s - con.rho_f) * con.g * (D(d) ^ 3)) / (con.rho_f * (con.nu ^ 2));
         if Da <= 0.05; % use Stokes for Da <= 0.05, from Dietrich, 1982 paper
             wa(d) = 1.71e-4 * (Da^2); 
-        elseif Da > 0.05 && Da <= 5e9; % use Dietrich if within valid range
-            wa(d) = DSV_fit(Da, CSF, Pow);
+        elseif Da > 0.05 && Da <= 5e9; % use Dietrich, 1982 only if within valid range
+            R_1 = (-3.76715) + (1.92944 * log10(Da)) - (0.09815 * (log10(Da)^(2.0))) - ...
+                (0.00575 * (log10(Da)^(3.0))) + (0.00056 * (log10(Da)^(4.0)));
+            R_2 = (log10(1 - ((1 - CSF) / 0.85))) - (((1 - CSF)^(2.3)) * tanh(log10(Da) - 4.6)) + ...
+                (0.3 * (0.5 - CSF) * ((1 - CSF)^(2.0)) * (log10(Da) - 4.6));
+            R_3 = (0.65 - ((CSF / 2.83) * tanh(log10(Da) - 4.6)))^(1 + (3.5 - Pow)/2.5);
+            wa(d) = R_3 * (10 ^ (R_1 + R_2));
         elseif Da > 5e9; % return warning and NaN for invalid range
             warning('%f \nsolution invalid for Da > 5e9 \nreturning NaN', Da)
             wa(d) = NaN; % no consideration to turbulence so invalid above this size
         end
-        ws(d) = get_ws(wa(d), con); % dimensionalize
+        ws(d) = ((wa(d) * (con.rho_s - con.rho_f) * con.g * con.nu) / con.rho_f)^(1/3);
     end
     
-end
-
-function [Da] = get_Da(D, con)
-    % non-dimensionalize the grain size
-    Da = ((con.rho_s - con.rho_f) * con.g * (D ^ 3)) / (con.rho_f * (con.nu ^ 2));
-end
-
-function [ws] = get_ws(wa, con)
-    % dimensionalize the settling velocity
-    ws = ((wa * (con.rho_s - con.rho_f) * con.g * con.nu) / con.rho_f)^(1/3);
-end
-
-function wa = DSV_fit(Da, CSF, Pow)
-    % calculate settling velocity by Dietrich, 1982
-    R_1 = (-3.76715) + (1.92944 * log10(Da)) - (0.09815 * (log10(Da)^(2.0))) - (0.00575 * (log10(Da)^(3.0))) + (0.00056 * (log10(Da)^(4.0)));
-    R_2 = (log10(1 - ((1 - CSF) / 0.85))) - (((1 - CSF)^(2.3)) * tanh(log10(Da) - 4.6)) + (0.3 * (0.5 - CSF) * ((1 - CSF)^(2.0)) * (log10(Da) - 4.6));
-    R_3 = (0.65 - ((CSF / 2.83) * tanh(log10(Da) - 4.6)))^(1 + (3.5 - Pow)/2.5);
-    wa = R_3 * (10 ^ (R_1 + R_2));
 end
