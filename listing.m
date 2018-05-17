2559ed9544c93c5324f1ca4e607e0bbc303b79fa
amoodie <amoodie@rice.edu>
2018-05-17 09:31:06 -0700

    working filesonly and dirsonly, should add an option for nohidden

--- listing.m
+++ listing.m
@@ -1,10 +1,11 @@
-function [list] = listing(directory, varargin)
+function [list_struct, list_names] = listing(directory, varargin)
     %listing gets the listing of all *real* items in a directory
     %
     % basically it strips the '.' and '..' folders. 
     % optionally will take 'dirsonly' or 'filesonly' or if any other string
     % or cell array of strings is given, it returns only files with those extensions.
     %
+    % returns structure of listing information and filename only listing
     
     if ~or(ischar(directory), isstring(directory))
         error('directory specification must be char or string')
@@ -24,15 +25,30 @@ function [list] = listing(directory, varargin)
     
     % if a cell array is given, filesonly is assumed
     % check cell array for all to be .*** file types
-    if any(cellfun(@iscell, varargin))
-        keepfiles ;
-        
+    varargcell = cellfun(@iscell, varargin);
+    if any( varargcell )
+        keepfiles(rawlist.isdir) = false; % logical of files only
     
     end
     
+    % if any non-cell argument is dirsonly
+    if any( cellfun(@(x) strcmp(x, 'dirsonly'), varargin(~varargcell)) )
+        keepdirs(~[rawlist.isdir]) = false; % logical of files only
+    end
     
-    filesBool = ~[rawlist.isdir]; % logical of files only
-    rawlist = rawlist(filesBool);
+    % if any non-cell argument is filesonly
+    if any( cellfun(@(x) strcmp(x, 'filesonly'), varargin(~varargcell)) )
+        keepfiles([rawlist.isdir]) = false; % logical of files only
+    end
+    
+    % process down to only files or dirs if desired
+    typelist = rawlist( and(keepdirs, keepfiles) );
+    
+    
+    % process the list down to only the types listed in a cell array if given.
+    % also check first if the argument supplied begins with a '.xxx' 
+    % then it is assumed to be a file type
+    
     listingnames = arrayfun(@(x) x.name, rawlist, 'Unif', 0);
     listingshoreline = cellfun(@(x) and(contains(x, '.mat'), contains(x, 'shoreline_')), listingnames);
     shorelinefiles = rawlist(listingshoreline);
