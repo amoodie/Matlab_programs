1d60bfc200da34ed2e3ff709cdb376382d96dff2
amoodie <amoodie@rice.edu>
2018-05-31 13:59:51 +0800

    finish documentation and fix function name

--- GGA2ll.m
+++ GGA2ll.m
@@ -1,4 +1,4 @@
-function [point, latmean, lonmean, zmean] = GPGGA2ll(filepath)
+function [point, latmean, lonmean, zmean] = GGA2ll(filepath)
     %GGA2ll reads a NMEA GGA string dump in filepath and processes to a single lat, lon, z point
     %
     % [point] = GPGGA2ll(filepath) takes a single file with a NMEA GGA string
@@ -50,20 +50,27 @@ function [point, latmean, lonmean, zmean] = GPGGA2ll(filepath)
         londeg = -londeg;
     end
     
-    %
+    % convert the minutes portion to a number
     latdecraw = cellfun(@(x) str2num( x(latstidx+1:end) ), latdecmin);
     londecraw = cellfun(@(x) str2num( x(lonstidx+1:end) ), londecmin);
     
+    % convert the minutes fraction to degrees fraction
     latdec = latdecraw / 60;
     londec = londecraw / 60;
     
+    % sum the degress and minutes fraction
     latdecdeg = latdeg + latdec;
     londecdeg = londeg + londec;
     
+    % convert elevation to a number
     znumeric = cellfun(@(x) str2num(x), zraw);
     
+    % average all the data in file
     latmean = mean(latdecdeg);
     lonmean = mean(londecdeg);
     zmean = mean(znumeric);
+    
+    % cat to single matrix
+    point = [latmean, lonmean, zmean];
     
 end
